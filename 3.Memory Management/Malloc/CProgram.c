d61b6a0816dfea864fa5b4ce4201fee0bbfafa9d
JMAT <jeenmathew26@gmail.com>
2020-06-01 22:38:04 +0530

    Memory Management with Malloc

---
+++ 3.Memory Management/Malloc/CProgram.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int* ptr;
+	int num,sum=0;
+	num=8;
+	printf("We have %d elements to allocate memory to\n",num);
+	
+	ptr=(int*)malloc(sizeof(int)*num);
+	if (ptr==NULL){
+			printf("Error! Could not Allocate Memory !!");
+			exit(0);
+	}
+	printf("Memory allocation has been succesfull");
+	
+	
+	for (int i=0 ; i<num ; ++i){
+			ptr[i]=i+1;
+	}
+	printf("\nInserted %d elements in the block are as follows:\n", num);
+	
+	for (int i=0 ;i<num ; ++i) {
+	
+		printf("\nPointer location: %d",&ptr[i]);		
+		printf("\nPointer Value: %d\n", ptr[i]);
+	}
+	
+}	
