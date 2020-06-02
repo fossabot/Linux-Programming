2bb3ebc5be9678929f03a328c9a0a6bedfdac80e
JMAT <jeenmathew26@gmail.com>
2020-06-02 22:12:30 +0530

    System Limits

---
+++ 7.System Limits/CProgram.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+	printf("\nBelow are the limits defined\n");
+	printf("\nLONG_MIN: %ld ",LONG_MIN);
+	
+	printf("\nLONG_MAX: %ld ",LONG_MAX);
+	
+	printf("\nCHAR_MIN: %d ",CHAR_MIN);
+	
+	printf("\nCHAR_MAX: %d ",CHAR_MAX);
+	
+	printf("\nCHAR_BIT: %d \n",CHAR_BIT);
+	
+	
+}	
