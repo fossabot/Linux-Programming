a792d0a2b707bef619d5012996ea2af9b85f01cd
JMAT <jeenmathew26@gmail.com>
2020-06-03 23:29:42 +0530

    Auto stash before merge of "master" and "origin/master"

---
+++ 14.Process Creation and Termination/exit/CProgram.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+
+
+int main() {
+	
+	printf("\n Jarvis says HEllo ! \n ");
+	
+	exit();
+	
+	printf("\nJarvis is going to sleep\n" )
+	
+	
+	
+}
+
